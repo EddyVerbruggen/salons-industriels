c2ec4184ec7d91ac08509c0f12fc17000c377ab2
Pascal Martineau <pascal@lewebsimple.ca>
2018-03-07 18:58:49 -0500

    Migrate to vue-cli-template

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Salons Industriels"/>
+
+    <StackLayout>
+        <Label>Bienvenue dans notre application mobile!</Label>
+    </StackLayout>
+
+  </Page>
+</template>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'nativescript-vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+const store = new Vuex.Store({
+  modules: {},
+  strict: debug,
+});
+
+Vue.prototype.$store = store;
+
+module.exports = store;
