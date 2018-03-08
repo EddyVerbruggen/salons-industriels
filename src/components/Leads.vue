28633fca43a84b82f45bc76d7f9694c09a653579
Pascal Martineau <pascal@lewebsimple.ca>
2018-03-07 20:27:41 -0500

    Initial leads ListView

--- src/components/Leads.vue
+++ src/components/Leads.vue
@@ -1,3 +1,7 @@
 <template>
-  <Label text="Leads"/>
+  <ListView for="item in $store.state.leads.items" @tap="editLead">
+    <v-template>
+      <Label :text="item.title"/>
+    </v-template>
+  </ListView>
 </template>
